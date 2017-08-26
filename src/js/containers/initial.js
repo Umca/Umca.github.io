46c71e551f11049586c872b59a2c6050d5f7a4be
Olya <olga.akinshyna@gmail.com>
2017-08-26 23:37:22 +0300

    save and submit logic added

--- src/js/containers/initial.js
+++ src/js/containers/initial.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import request from '../utils/request';
+import store from '../utils/store';
+import SubmitButton from '../components/submit';
+import RadioButton from '../components/radio';
 
 class InitialComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            checked: "user",
+            data: "",
+            response: null,
+            errors: null
+        }
+    }
+    handleRadioChanged(e){
+        this.setState({
+            checked: e.target.value
+        })
+    }
+    handleInputChange(e){
+        this.setState({
+            data: e.target.value
+        })
+    }
     changeRoute(){
         this.props.history.push(`/${this.state.data}`);
     }
+    handleSubmit(e){
+        e.preventDefault();
+        this.url = `https://api.github.com/${this.state.checked}s/${this.state.data}`;
+        request
+            .send(this.url)
+            .then(response => {
+                this.setState({
+                    response
+                });
+                this.saveDataToStore();
+                this.changeRoute();
+            })
+            .catch(err => {
+                this.setState({
+                    errors : err
+                });
+                this.saveDataToStore();
+            })
+    }
+    
+
+    saveDataToStore(){
+        for(let key in this.state){
+            store.add(this.state[key], key)
+        }
+    }
+
     render() {
         return (
             <div className="container">
