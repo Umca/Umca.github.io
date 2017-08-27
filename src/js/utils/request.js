5b85f3377595f41ff6a6e4ee09225b3e6fbb8fa1
Olya <olga.akinshyna@gmail.com>
2017-08-27 16:41:35 +0300

    error handling added

--- src/js/utils/request.js
+++ src/js/utils/request.js
@@ -1,7 +1,18 @@
 const request = {
     send(url){
-        return fetch(url)
-            .then(res => res.json())
+
+        let headers = new Headers();
+        headers.append('Accept', 'application/vnd.github.mercy-preview+json');
+
+        return fetch(url, {
+            headers: headers
+        })
+            .then(res => {
+                if(res.message !== 'Not found'){
+                    return res.json();
+                }
+                throw new Error ('There is now such user / org!');
+            })
     }
 }
 
