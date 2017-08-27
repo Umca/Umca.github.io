f7e80180bbe12e20395e5ee7ea0208f8df1348ee
Olya <olga.akinshyna@gmail.com>
2017-08-28 01:22:48 +0300

    new routes added

--- src/js/containers/app.js
+++ src/js/containers/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import  InitialComponent  from './initial';
+import Main from './main';
 
 const  App = () => (
     <Router>
-        <Route path='/' component={InitialComponent} />    
+        <Switch>
+        <Route exact path='/' component={InitialComponent} />
+        <Route path='/:user' component={Main} /> 
+        </Switch>   
     </Router>
 );
 
