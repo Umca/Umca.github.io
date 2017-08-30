8207bb3d6b4ec21d656b135575c513db80ac55f6
Olya <olga.akinshyna@gmail.com>
2017-08-31 00:34:33 +0300

    repos rerender when sorting added

--- src/js/containers/main.js
+++ src/js/containers/main.js
@@ -6,6 +6,7 @@ import Cards from './cards';
 import moment from 'moment';
 import ee from '../utils/ee';
 import filter from '../utils/filter';
+import sorting from '../utils/sort';
 
 window.moment = moment;
 
@@ -13,9 +14,14 @@ export default class Main extends React.Component{
     constructor(){
         super();
         this.repos = JSON.parse(localStorage.getItem('store')) ? JSON.parse(localStorage.getItem('store')) .repos : []
-        console.log(this.repos)
         this.state = {
             filtered: this.repos
+        }
+        this.types = {
+            name: 'string',
+            stargazers_count: 'number',
+            open_issues_count: 'number',
+            updated_at: 'date'
         }
         this.addEeListener();
     }
@@ -32,6 +38,14 @@ export default class Main extends React.Component{
             let filtered = filter.filter(this.repos, obj);
             this.setState({
                 filtered
+            })
+        })
+
+        ee.on('apply_sorting', obj => {
+            let { sort_type, order } = obj;
+            let sorted = sorting.sort(this.state.filtered, sort_type, this.types[sort_type], order);
+            this.setState({
+                filtered: sorted
             })
         })
     }
