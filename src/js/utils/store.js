1f943175ce7a887114e414cef6d120bdf5e48789
Olya <olga.akinshyna@gmail.com>
2017-09-03 16:23:17 +0300

    store functionality changed

--- src/js/utils/store.js
+++ src/js/utils/store.js
@@ -1,22 +1,9 @@
 const store = {
-    data:{},
-    add(value, field){
-        if(!this.data.field){
-            this.data[field] = value;
-        } else {
-            this.data[field] = value;
-        }
+    save(key, value) {
+        localStorage.setItem(key, JSON.stringify(value))
     },
-    remove(field){
-        if(!this.data[field]){
-            console.warn('There is no such piece if data!')
-        } else {
-            delete this.data[field];
-            console.log(`${this.data[field]} is deleted successfully!`)
-        }
-    },
-    save(){
-        localStorage.setItem('store', JSON.stringify(this.data))
+    extract(key) {
+        return JSON.parse(localStorage.getItem(key))
     }
 };
 
