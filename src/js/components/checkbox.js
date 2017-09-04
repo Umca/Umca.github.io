01f731333b110a731cd199e73e07c982e0d80191
Olya <olga.akinshyna@gmail.com>
2017-09-04 23:49:23 +0300

    checked style

--- src/js/components/checkbox.js
+++ src/js/components/checkbox.js
@@ -4,17 +4,19 @@ import '../../styles/parts.css';
 const Checkbox = (props) => {
     return (
         <div>
-            <label className="control control-checkbox"
+            <label className="control control-checkbox "  
                 onClick={props.onChange}
-                checked={props.checked}
+                checked={props.isChecked}
                 
             >
                 
                 <input type="checkbox" />
                 <span
-                    className="label-text"    
+                    className={"label-text " + (props.isChecked ? 'checked-text' : '')}    
                 >{props.label}</span>    
-            <div className="control_indicator"></div>
+                <div className={"control_indicator " + (props.isChecked ? 'checked-control-indicator' : '')}>
+                    {props.isChecked ? <div className="fake-checked"></div> : null}
+                </div>
         </label>
         </div>    
     )
